b90ca97f7d7e7c03092714dc45d77a0db8258093
sergei.winitzki <sergei.winitzki@workday.com>
2023-10-29 17:09:46 +0100

    wip failing substitution test

--- scall-core/src/test/scala/io/chymyst/ui/dhall/unit/SimpleSemanticsTest.scala
+++ scall-core/src/test/scala/io/chymyst/ui/dhall/unit/SimpleSemanticsTest.scala
@@ -2,11 +2,26 @@ package io.chymyst.ui.dhall.unit
 
 import com.eed3si9n.expecty.Expecty.expect
 import io.chymyst.ui.dhall.Semantics
+import io.chymyst.ui.dhall.Semantics.substitute
 import munit.FunSuite
 import io.chymyst.ui.dhall.Syntax.Expression._
+import io.chymyst.ui.dhall.Syntax.ExpressionScheme.{Variable, underscore}
 import io.chymyst.ui.dhall.SyntaxConstants.Builtin.Natural
+import io.chymyst.ui.dhall.SyntaxConstants.VarName
 
 class SimpleSemanticsTest extends FunSuite {
+
+  test("substitute in a variable") {
+    val variable =  v("x")
+    val result = Semantics.substitute(variable, VarName("x"), 0, Variable(underscore, 0))
+    expect(result.toDhall == "_@0")
+  }
+
+  test("substitute in a lambda") {
+    val lam = (v("y") | ~Natural) -> v("x")
+    val result = Semantics.substitute(lam, VarName("x"), 0, Variable(underscore, 0))
+    expect(result.toDhall == "\\(y : Natural) -> _@0")
+  }
 
   test("alpha-normalize a nested lambda") {
     val nested = (v("x") | ~Natural) -> ((v("y") | ~Natural) -> v("x"))
