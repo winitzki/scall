7d11bc7f8018f078758d4c9c12ff10c6810142ef
sergei.winitzki <sergei.winitzki@workday.com>
2023-11-02 21:11:32 +0100

    cleanup

--- scall-core/src/main/scala/io/chymyst/ui/dhall/Imports.scala
+++ scall-core/src/main/scala/io/chymyst/ui/dhall/Imports.scala
@@ -20,17 +20,24 @@ object Imports {
   def corsHeader = "Access-Control-Allow-Origin"
 
   // If `None` there is no error.
-  def corsComplianceError(parent: ImportType[Expression], child: ImportType[Expression], headers: Map[String, Seq[String]]): Option[String] = (parent, child) match {
+  def corsComplianceError(parent: ImportType[Expression], child: ImportType[Expression], responseHeaders: Map[String, Seq[String]]): Option[String] = (parent, child) match {
     // TODO: report issue: what if parent = Remote but child = Path, does the cors judgment then always fail?
     case (Remote(URL(scheme1, authority1, path1, query1), headers1), Remote(URL(scheme2, authority2, path2, query2), headers2)) =>
       if (scheme1 == scheme2 && authority1 == authority2) None
-      else headers.get(corsHeader) match {
+      else responseHeaders.get(corsHeader) match {
         case Some(Seq("*")) => None
         case Some(Seq(other)) if other.toLowerCase == s"$scheme2://$authority2".toLowerCase => None
-        case Some(other) => Some(s"Scheme or authority differs from parent $parent but CORS headers in child $child is $headers and does not allow importing")
-        case None => Some(s"Scheme or authority differs from parent $parent but no CORS header in child $child, headers $headers")
+        case Some(_) => Some(s"Scheme or authority differs from parent $parent but CORS headers in child $child is $responseHeaders and does not allow importing")
+        case None => Some(s"Scheme or authority differs from parent $parent but no CORS header in child $child, headers $responseHeaders")
       }
-    case (Remote(URL(scheme1, authority1, path1, query1), headers1), _) => Some(s"Remote parent $parent may not import a non-remote $child")
-    case _s => None
+    case (Remote(URL(_, _, _, _), _), _) => Some(s"Remote parent $parent may not import a non-remote $child")
+    case _ => None
+  }
+
+  final case class ImportContext(resolved: Map[Import[Expression], Expression])
+
+  // TODO report issue: in imports.md probably `Γ(headersPath) = userHeadersExpr` should be rewritten as `userHeadersExpr = Γ(headersPath)`
+  def resolveImports(visited: Seq[Import[Expression]] /* non-empty */ , gamma: ImportContext, expr: Expression): (Expression, ImportContext) = {
+    ???
   }
 }
