ab7a1d1827429b41f2d96f39519a74eecc055092
sergei.winitzki <sergei.winitzki@workday.com>
2023-11-09 22:16:43 +0100

    wip import tests

--- scall-core/src/test/scala/io/chymyst/ui/dhall/unit/DhallImportResolutionSuite.scala
+++ scall-core/src/test/scala/io/chymyst/ui/dhall/unit/DhallImportResolutionSuite.scala
@@ -15,7 +15,7 @@ import scala.util.Try
 
 class DhallImportResolutionSuite extends FunSuite with OverrideEnvironment with ResourceFiles {
 
-  def setupEnvironment[R](extraEnvVars: (String, String)*)(code: => R): R = {
+  def setupEnvironment[R](code: => R): R = {
     val tempDir = os.temp.dir(root / "tmp", deleteOnExit = true)
     try {
       os.copy(from = os.Path(resourceAsFile("dhall-lang/tests/import/cache").get.getAbsolutePath), to = tempDir, replaceExisting = true)
@@ -25,42 +25,45 @@ class DhallImportResolutionSuite extends FunSuite with OverrideEnvironment with 
         "DHALL_TEST_VAR" -> "6 * 7",
         "XDG_CACHE_HOME" -> tempDir.toNIO.toAbsolutePath.toString,
         "HOME" -> dhallHome,
-      ) ++ extraEnvVars
+      )
       runInFakeEnvironmentWith(envVars: _*)(code)
     } finally os.remove(tempDir)
   }
 
   test("import resolution success") {
-    val results: Seq[Try[String]] = enumerateResourceFiles("dhall-lang/tests/import/success", Some("A.dhall"))
-      .map { file =>
-        val validationFile = new File(file.getAbsolutePath.replace("A.dhall", "B.dhall"))
-        val envVarsFile = new File(file.getAbsolutePath.replace("A.dhall", "ENV.dhall"))
-        val extraEnvVars: Seq[(String, String)] = if (envVarsFile.exists) {
-          val Parsed.Success(DhallFile(_, envs), _) = Parser.parseDhallStream(new FileInputStream(envVarsFile))
-          envs.toPrimitiveValue match {
-            case Some(t: List[Map[String, AnyRef]]) => t.flatMap {
-              case x: Map[String, AnyRef] if x.keys.toList.sorted == List("mapKey", "mapValue") && x.values.forall(_.isInstanceOf[String]) =>
-                Some(x("mapKey").asInstanceOf[String] -> x("mapValue").asInstanceOf[String])
-              case _ => None
+    setupEnvironment {
+      val results: Seq[Try[String]] = enumerateResourceFiles("dhall-lang/tests/import/success", Some("A.dhall"))
+        .map { file =>
+          val validationFile = new File(file.getAbsolutePath.replace("A.dhall", "B.dhall"))
+          val envVarsFile = new File(file.getAbsolutePath.replace("A.dhall", "ENV.dhall"))
+          val extraEnvVars: Seq[(String, String)] = if (envVarsFile.exists) {
+            val Parsed.Success(DhallFile(_, envs), _) = Parser.parseDhallStream(new FileInputStream(envVarsFile))
+            envs.toPrimitiveValue match {
+              case Some(t: List[Map[String, AnyRef]]) => t.flatMap {
+                case x: Map[String, AnyRef] if x.keys.toList.sorted == List("mapKey", "mapValue") && x.values.forall(_.isInstanceOf[String]) =>
+                  Some(x("mapKey").asInstanceOf[String] -> x("mapValue").asInstanceOf[String])
+                case _ => None
+              }
+              case None => Seq()
             }
-            case None => Seq()
+          } else Seq()
+          runInFakeEnvironmentWith(extraEnvVars:_*) {
+            val result = Try {
+              val Parsed.Success(DhallFile(_, ourResult), _) = Parser.parseDhallStream(new FileInputStream(file))
+              val Parsed.Success(DhallFile(_, validationResult), _) = Parser.parseDhallStream(new FileInputStream(validationFile))
+              // resolve from ./dhall-lang/tests/...dhall as parent
+              val x = ourResult.resolveImports(???)
+              val y = validationResult.resolveImports(???)
+              expect(x.toDhall == y.toDhall && x == y)
+              file.getName
+            }
+            if (result.isFailure) println(s"${file.getName}: ${result.failed.get.getMessage}")
+            result
           }
-        } else Seq()
-        ???
-        val result = Try {
-          val Parsed.Success(DhallFile(_, ourResult), _) = Parser.parseDhallStream(new FileInputStream(file))
-          val Parsed.Success(DhallFile(_, validationResult), _) = Parser.parseDhallStream(new FileInputStream(validationFile))
-          val x = ourResult.alphaNormalized
-          val y = validationResult.alphaNormalized
-          //  println(s"DEBUG: ${file.getName}: our parser gives ${ourResult.toDhall}, after alpha-normalization ${x.toDhall}")
-          expect(x.toDhall == y.toDhall && x == y)
-          file.getName
         }
-        if (result.isFailure) println(s"${file.getName}: ${result.failed.get.getMessage}")
-        result
-      }
-    println(s"Success count: ${results.count(_.isSuccess)}\nFailure count: ${results.count(_.isFailure)}")
-    expect(results.count(_.isFailure) == 0)
+      println(s"Success count: ${results.count(_.isSuccess)}\nFailure count: ${results.count(_.isFailure)}")
+      expect(results.count(_.isFailure) == 0)
+    }
   }
 
   test("import resolution failure") {
