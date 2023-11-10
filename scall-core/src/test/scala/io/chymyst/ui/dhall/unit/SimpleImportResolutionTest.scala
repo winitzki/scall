587f9225608efd7008619a4bbf190dd57e299d54
sergei.winitzki <sergei.winitzki@workday.com>
2023-11-10 11:56:09 +0100

    wip import chaining tess

--- scall-core/src/test/scala/io/chymyst/ui/dhall/unit/SimpleImportResolutionTest.scala
+++ scall-core/src/test/scala/io/chymyst/ui/dhall/unit/SimpleImportResolutionTest.scala
@@ -3,12 +3,13 @@ package io.chymyst.ui.dhall.unit
 import com.eed3si9n.expecty.Expecty.expect
 import fastparse.Parsed
 import io.chymyst.test.ResourceFiles.enumerateResourceFiles
+import io.chymyst.ui.dhall.Parser.InlineDhall
 import io.chymyst.ui.dhall.Syntax.ExpressionScheme._
 import io.chymyst.ui.dhall.Syntax.{DhallFile, Expression}
-import io.chymyst.ui.dhall.SyntaxConstants.{Builtin, ConstructorName, FieldName, VarName}
+import io.chymyst.ui.dhall.SyntaxConstants.{Builtin, ConstructorName, FieldName, FilePrefix, ImportType, VarName}
 import io.chymyst.ui.dhall.TypeCheck._Type
 import io.chymyst.ui.dhall.TypeCheckResult.Valid
-import io.chymyst.ui.dhall.{Parser, TypeCheckResult}
+import io.chymyst.ui.dhall.{Parser, SyntaxConstants, TypeCheckResult}
 import munit.FunSuite
 
 import java.io.FileInputStream
@@ -19,13 +20,25 @@ class SimpleImportResolutionTest extends FunSuite {
     enumerateResourceFiles("dhall-lang/tests/import/success", Some("originHeadersENV.dhall"))
       .foreach { file =>
         val envs = DhallImportResolutionSuite.readHeadersFromEnv(file)
-        expect(envs == Seq(("DHALL_HEADERS", """toMap {
-                                               |  `httpbin.org:443` = toMap {
-                                               |    `User-Agent` = "Dhall"
-                                               |  }
-                                               |}
-                                               |""".stripMargin)))
+        expect(envs == Seq(("DHALL_HEADERS",
+          """toMap {
+            |  `httpbin.org:443` = toMap {
+            |    `User-Agent` = "Dhall"
+            |  }
+            |}
+            |""".stripMargin)))
       }
+  }
+
+  test("import chaining must compute correct paths") {
+    val import1 = "/tmp/file1.dhall".dhall.scheme.asInstanceOf[Import[Expression]]
+    val import2 = "/tmp/file2.dhall".dhall.scheme.asInstanceOf[Import[Expression]]
+    val chained = import1 chainWith import2
+    expect(chained.importType == ImportType.Path(FilePrefix.Absolute, SyntaxConstants.File(Seq("tmp", "file2.dhall"))))
+
+    val import3 = "../file3.dhall".dhall.scheme.asInstanceOf[Import[Expression]]
+    val chained13 = import1 chainWith import3
+    expect(chained13.canonicalize.importType == ImportType.Path(FilePrefix.Absolute, SyntaxConstants.File(Seq("tmp", "file3.dhall"))))
   }
 
 }
