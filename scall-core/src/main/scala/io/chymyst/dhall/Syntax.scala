64f179c5215d903b6e62155ce5e8ea881587b774
sergei.winitzki <sergei.winitzki@workday.com>
2023-11-22 13:38:24 +0100

    non-lazy local time

--- scall-core/src/main/scala/io/chymyst/dhall/Syntax.scala
+++ scall-core/src/main/scala/io/chymyst/dhall/Syntax.scala
@@ -661,7 +661,7 @@ object Syntax {
         */
       val nanosTruncated: Int = (nanosPrinted.take(9) + "0" * 9).take(9).toInt
 
-      lazy val toLocalTime: LocalTime = LocalTime.of(hours, minutes, seconds, nanosTruncated)
+      val toLocalTime: LocalTime = LocalTime.of(hours, minutes, seconds, nanosTruncated)
 
       override def toString: String = f"$hours%02d:$minutes%02d:$seconds%02d${if (nanosPrinted.isEmpty) "" else "." + nanosPrinted}"
     }
