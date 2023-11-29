b0f67f39cc9c459f9672de9e59a418c956072afb
sergei.winitzki <sergei.winitzki@workday.com>
2023-11-29 13:51:04 +0100

    fix tests

--- scall-core/src/test/scala/io/chymyst/dhall/unit/SimpleSemanticsTest.scala
+++ scall-core/src/test/scala/io/chymyst/dhall/unit/SimpleSemanticsTest.scala
@@ -19,13 +19,13 @@ class SimpleSemanticsTest extends DhallTest {
   test("substitute in a lambda") {
     val lam    = (v("y") | ~Natural) -> v("x")
     val result = Semantics.substitute(lam, VarName("x"), 0, Variable(underscore, 0))
-    expect(result.toDhall == "λ(y : Natural) -> _")
+    expect(result.toDhall == "λ(y : Natural) → _")
   }
 
   test("alpha-normalize a nested lambda") {
     val nested = (v("x") | ~Natural) -> ((v("y") | ~Natural) -> v("x"))
-    expect(nested.toDhall == "λ(x : Natural) -> λ(y : Natural) -> x")
-    expect(nested.alphaNormalized.toDhall == "λ(_ : Natural) -> λ(_ : Natural) -> _@1")
+    expect(nested.toDhall == "λ(x : Natural) → λ(y : Natural) → x")
+    expect(nested.alphaNormalized.toDhall == "λ(_ : Natural) → λ(_ : Natural) → _@1")
   }
 
   test("alpha-normalize record access") {
