f8cd5c3123535ae889ea2c7f1b6631d6f8d8f39a
sergei.winitzki <sergei.winitzki@workday.com>
2023-12-11 21:48:29 +0100

    more tests

--- dhall-codec/src/test/scala/io/chymyst/dhall/unit/ToScalaTest.scala
+++ dhall-codec/src/test/scala/io/chymyst/dhall/unit/ToScalaTest.scala
@@ -45,6 +45,7 @@ class ToScalaTest extends FunSuite {
   test("convert type names to Scala type tags") {
     expect("Text".dhall.asScala[Tag[String]] == Tag[String])
     expect("Bool".dhall.asScala[Tag[Boolean]] == Tag[Boolean])
+    expect("Bytes".dhall.asScala[Tag[Array[Byte]]] == Tag[Array[Byte]])
     expect("Integer".dhall.asScala[Tag[BigInt]] == Tag[BigInt])
     expect("Natural".dhall.asScala[Tag[BigInt]] == Tag[BigInt])
     expect("Time".dhall.asScala[Tag[LocalTime]] == Tag[LocalTime])
@@ -63,6 +64,19 @@ class ToScalaTest extends FunSuite {
     expect(Natural_odd(2) == false && Natural_even(2) == true)
     expect("Natural/show".dhall.asScala[Natural => String] == Natural_show)
     expect("Natural/show 2".dhall.typeCheckAndBetaNormalize().unsafeGet.asScala[String] == "2")
+    expect("Natural/subtract".dhall.asScala[Natural => Natural => Natural] == Natural_subtract)
+    expect("Natural/isZero".dhall.asScala[Natural => Boolean] == Natural_isZero)
+    expect("Natural/toInteger".dhall.asScala[Natural => BigInt] == Natural_toInteger)
+//    expect("Natural/subtract 1 20".dhall.asScala[Natural].intValue == 19) // TODO enable this test
+    expect("Double/show".dhall.asScala[Double => String] == Double_show)
+    expect("Text/show".dhall.asScala[String => String] == Text_show)
+    expect("Integer/show".dhall.asScala[BigInt => String] == Integer_show)
+    expect("Integer/negate".dhall.asScala[BigInt => BigInt] == Integer_negate)
+    expect("Integer/clamp".dhall.asScala[BigInt => Natural] == Integer_clamp)
+    expect("Date/show".dhall.asScala[LocalDate => String] == Date_show)
+    expect("Time/show".dhall.asScala[LocalTime => String] == Time_show)
+    expect("TimeZone/show".dhall.asScala[ZoneOffset => String] == TimeZone_show)
+
   }
 
   test("fail to convert imports") {
@@ -82,11 +96,13 @@ class ToScalaTest extends FunSuite {
   }
 
   test("lists") { // TODO enable this test and fix type tags. The error is: Error importing from Dhall: type mismatch: expected type Tag[Seq[+Boolean]] but Dhall value actually has type Valid(List Bool) and type tag Tag[List[+Tag[=Boolean]]]
-    expect("[True, False]".dhall.asScala[Seq[Tag[Boolean]]] == Seq(true, false))
+    expect(Tag.appliedTag(TagK[List], List(Tag[String].tag)) == Tag[List[String]])
     expect("[]: List Text".dhall.asScala[Seq[Tag[String]]] == Seq())
+
+    expect("[True, False]".dhall.asScala[Seq[Tag[Boolean]]] == Seq(true, false))
     expect(
       "[1, 2] # [3, 4]".dhall.asScala[Seq[_]] == Seq(1, 2, 3, 4).map(BigInt(_))
-    )             // TODO: fix type. This should accept asScala[Seq[Natural]] but currently it does not work.
+    ) // TODO: fix type. This should accept asScala[Seq[Natural]] but currently it does not work.
   }
 
   test("Optional") {
