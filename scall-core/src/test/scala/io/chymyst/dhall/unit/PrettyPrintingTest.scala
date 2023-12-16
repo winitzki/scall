cea8ff716a0082606e9fbd32cd3f9910561a7d9e
sergei.winitzki <sergei.winitzki@workday.com>
2023-12-16 08:55:13 +0100

    wip failing test

--- scall-core/src/test/scala/io/chymyst/dhall/unit/PrettyPrintingTest.scala
+++ scall-core/src/test/scala/io/chymyst/dhall/unit/PrettyPrintingTest.scala
@@ -13,4 +13,11 @@ class PrettyPrintingTest extends DhallTest {
   test("print assert expressions") {
     expect("let x=1===1 in assert: x".dhall.typeCheckAndBetaNormalize().unsafeGet.print == "assert : 1 ≡ 1")
   }
+
+  test("print long expressions without stack overflow") {
+    (1 to 1000 by 50).foreach { n =>
+      println(s"Iteration $n")
+      expect(("1" + " + 1" * n).dhall.print.length == 4 * n + 1)
+    }
+  }
 }
