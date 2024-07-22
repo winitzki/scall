6abdf9e7ee1edc12d15597f4b65336b82eca7662
Sergei Winitzki <winitzki@gmail.com>
2024-07-22 22:38:55 +0200

    wip

--- nano-dhall/src/main/scala/io/chymyst/nanodhall/SymbolicGraph.scala
+++ nano-dhall/src/main/scala/io/chymyst/nanodhall/SymbolicGraph.scala
@@ -3,33 +3,43 @@ package io.chymyst.nanodhall
 import sourcecode.{File, Line, Name}
 
 object SymbolicGraph {
-  final   class RuleDef(val name: String, rule:  => GrammarRule) {
+  final class RuleDef(val name: String, rule: () => GrammarRule) {
     override def equals(obj: Any): Boolean = obj match {
       case r: RuleDef => name == r.name
       case _          => false
     }
-    lazy val grammarRule : GrammarRule = rule
+    def grammarRule: GrammarRule      = rule()
   }
 
   sealed trait GrammarRule
-  final case class LiteralMatch(text: String) extends GrammarRule
-  final case class GrammarSymbol(  name: String, rule: () => RuleDef) extends GrammarRule {
+  final case class LiteralMatch(text: String)                       extends GrammarRule
+  final case class GrammarSymbol(name: String, rule: () => RuleDef) extends GrammarRule {
     override def equals(obj: Any): Boolean = obj match {
-      case  GrammarSymbol(`name`, _)  => true
-      case _                => false
+      case GrammarSymbol(`name`, _) => true
+      case _                        => false
     }
 
   }
   final case class And(rule1: GrammarRule, rule2: GrammarRule) extends GrammarRule
-  final case class Or(rule1: GrammarRule, rule2: GrammarRule) extends GrammarRule
+  final case class Or(rule1: GrammarRule, rule2: GrammarRule)  extends GrammarRule
 
   implicit class SymbolicGraphOps(r: => RuleDef) {
     def ~(next: => RuleDef)(implicit file: File, line: Line, varName: Name): RuleDef =
-      new RuleDef(name = varName.value, rule =   And(r.grammarRule, next.grammarRule))
+      new RuleDef(name = varName.value, rule = {() =>
+        println(s"DEBUG: evaluating And(${r.name}, ${next.name}")
+        And(r.grammarRule,
+          next.grammarRule)
+      })
 
-    def |(next: => RuleDef)(implicit file: File, line: Line, varName: Name): RuleDef = new RuleDef(name = varName.value, rule =  Or(r.grammarRule, next.grammarRule))
+    def |(next: => RuleDef)(implicit file: File, line: Line, varName: Name): RuleDef =
+      new RuleDef(name = varName.value, rule =
+  {        () =>
+    println(s"DEBUG: evaluating Or(${r.name}, ${next.name}")
+    Or(r.grammarRule,
+          next.grammarRule)
+  })
   }
 
-  def lit(s: String)(implicit file: File, line: Line, varName: Name): RuleDef = new RuleDef(name = varName.value, rule =   LiteralMatch(s))
+  def lit(s: String)(implicit file: File, line: Line, varName: Name): RuleDef = new RuleDef(name = varName.value, rule = () => LiteralMatch(s))
 
 }
--- nano-dhall/src/test/scala/io/chymyst/nanodhall/unit/SymbolicGraphTest.scala
+++ nano-dhall/src/test/scala/io/chymyst/nanodhall/unit/SymbolicGraphTest.scala
@@ -6,17 +6,40 @@ import munit.FunSuite
 
 class SymbolicGraphTest extends FunSuite {
 
+  test("grammar without circular dependencies") {
+    def a: RuleDef = lit("x")
+    def b: RuleDef = lit("y") ~ a
+    def c: RuleDef = lit("y") ~ a | b
+
+    expect(a.name == "a")
+    expect(a.grammarRule  match {
+      case LiteralMatch("x") => true
+    })
+
+    expect(b.name == "b")
+    expect(b.grammarRule match {
+      case And(LiteralMatch("y"), GrammarSymbol("a", _)) => true
+    })
+  }
+
   test("circular dependencies do not create an infinite loop 1") {
 
     def a: RuleDef = lit("x")
 
-    def b: RuleDef = lit("y") ~ b | lit("z")
+    def b: RuleDef = (lit("y") ~ b) | lit("z")
 
     expect(a.name == "a")
     expect(b.name == "b")
 
-    expect(a == new RuleDef(name = "a", rule =   LiteralMatch("x")))
-    expect(b == new RuleDef(name = "b", rule =   Or(And(LiteralMatch("y"),   GrammarSymbol("b", () => b)), LiteralMatch("z"))))
+    expect(a.grammarRule match {
+      case LiteralMatch("x") => true
+    })
+
+    b.grammarRule
+
+    expect(b.grammarRule match {
+      case Or(And(LiteralMatch("y"), GrammarSymbol("b", _)), LiteralMatch("z")) => true
+    })
   }
 
   test("circular dependencies do not create an infinite loop 2") {
@@ -31,13 +54,10 @@ class SymbolicGraphTest extends FunSuite {
     expect(b.name == "b")
     expect(c.name == "c")
 
-    expect(a == new RuleDef(name = "a", rule =  And(  GrammarSymbol("b", () => b),   GrammarSymbol("c", () => c))))
-    expect(
-      b == new RuleDef(
-        name = "b",
-        rule =  Or(And(And(LiteralMatch("x"),   GrammarSymbol("a", () => a)),   GrammarSymbol("b", () => b)), LiteralMatch("y")),
-      )
-    )
-    expect(c == new RuleDef(name = "c", rule =   And(LiteralMatch("z"),   GrammarSymbol("a", () => a))))
+//    expect(a == new RuleDef(name = "a", rule = () => And(GrammarSymbol("b", () => b), GrammarSymbol("c", () => c))))
+//    expect(
+//      b == new RuleDef(name = "b", rule = () => Or(And(And(LiteralMatch("x"), GrammarSymbol("a", () => a)), GrammarSymbol("b", () => b)), LiteralMatch("y")))
+//    )
+//    expect(c == new RuleDef(name = "c", rule = () => And(LiteralMatch("z"), GrammarSymbol("a", () => a))))
   }
 }
