096682115e6999309cfe604cc64516f4ed4872f6
Sergei Winitzki <winitzki@gmail.com>
2024-12-13 17:16:39 +0100

    wip

--- nano-dhall/src/test/scala/io/chymyst/nanodhall/unit/NanoDhallGrammarTest.scala
+++ nano-dhall/src/test/scala/io/chymyst/nanodhall/unit/NanoDhallGrammarTest.scala
@@ -26,21 +26,23 @@ class NanoDhallGrammarTest extends FunSuite {
   }
 
   test("parse lambda") {
-    expect(parser1.parse("\\(x : Natural) -> x + 1") == Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Variable(VarName("x"), 0), NanoOperator.Plus, NaturalLiteral(1))))
+    expect(parser1.parse("\\(x : Natural) -> x + 1") == Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Variable(VarName("x"), BigInt(0)), NanoOperator.Plus, NaturalLiteral(BigInt(1)))))
   }
 
 
   test("parse lambda and applications") {
-    expect(parser1.parse("(\\(x : Natural) -> f x + 1) 2") == Application(Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Application(Variable(VarName("f"), 0), Variable(VarName("x"), 0)), NanoOperator.Plus, NaturalLiteral(1))), NaturalLiteral(2)))
+    expect(NaturalLiteral(0) == NaturalLiteral(BigInt(0)))
+    expect(parser1.parse("(\\(x : Natural) -> f x + 1) 2") == Application(Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Application(Variable(VarName("f"), BigInt(0)), Variable(VarName("x"), BigInt(0))), NanoOperator.Plus, NaturalLiteral(BigInt(1)))), NaturalLiteral(BigInt(2))))
   }
 
   test("parse forall") {
-    expect(parser1.parse("(\\(x : Natural) -> x + 1) : forall (x : Natural) -> Natural") == Annotation(Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Variable(VarName("x"), 0), NanoOperator.Plus, NaturalLiteral(1))), Forall(VarName("x"), Constant(NanoConstant.Natural), Constant(NanoConstant.Natural))))
+    expect(parser1.parse("(\\(x : Natural) -> x + 1) : forall (x : Natural) -> Natural") == Annotation(Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Variable(VarName("x"), BigInt(0)), NanoOperator.Plus, NaturalLiteral(BigInt(1)))), Forall(VarName("x"), Constant(NanoConstant.Natural), Constant(NanoConstant.Natural))))
   }
 
-  test("parse let-binding") {
+  test("parse let-bindings") {
     val expected = Let(VarName("x"), NaturalLiteral(BigInt(1)), Let(VarName("y"), Variable(VarName("x"), BigInt(0)), Variable(VarName("y"), BigInt(0))))
     expect(parser1.parse("let x = 1 in let y = x in y") == expected)
     expect(parser1.parse("let x = 1 let y = x in y") == expected)
   }
+
 }
