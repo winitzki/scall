ae93621c99d4047fb324b88845b6f573980e7ff3
Sergei Winitzki <winitzki@gmail.com>
2024-12-13 17:11:12 +0100

    wip

--- nano-dhall/src/main/scala/io/chymyst/nanodhall/NanoDhallParser.scala
+++ nano-dhall/src/main/scala/io/chymyst/nanodhall/NanoDhallParser.scala
@@ -4,19 +4,15 @@ import fastparse.Parsed
 import io.chymyst.fastparse.Memoize
 
 final case class NanoDhallParser[R <: NanoExpr[R]](create: NanoExpr[R]) {
-  implicit class StringAsDhallExpression(val input: String) {
-    def dhall(create: NanoExpr[R]): R = parseToExpression(input)
-  }
 
-  def parseToExpression(input: String): R = parseDhall(input) match {
+  def parse(input: String): R = parseToResult(input) match {
     case Parsed.Success(value: R, index) => value
     case failure: Parsed.Failure =>
       Memoize.clearAll() // Parser will be re-run on trace(). So, the parser cache needs to be cleared.
       throw new Exception(s"Dhall parser error: ${failure.extra.trace().longMsg}")
   }
 
-
-  def parseDhall(source: String): Parsed[R] = Memoize.parse(source, NanoDhallGrammar(create).complete_expression(_))
+  def parseToResult(source: String): Parsed[R] = Memoize.parse(source, NanoDhallGrammar(create).complete_expression(_))
 
 }
 
--- nano-dhall/src/test/scala/io/chymyst/nanodhall/unit/NanoDhallGrammarTest.scala
+++ nano-dhall/src/test/scala/io/chymyst/nanodhall/unit/NanoDhallGrammarTest.scala
@@ -10,31 +10,31 @@ class NanoDhallGrammarTest extends FunSuite {
   val parser1 = NanoDhallParser.adtParser
 
   test("parse natural number") {
-    expect(parser1.parseToExpression("0") == NaturalLiteral(BigInt(0)))
-    expect(parser1.parseToExpression("123") == NaturalLiteral(BigInt(123)))
-    expect(parser1.parseToExpression("123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890") == NaturalLiteral(BigInt("123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890")))
+    expect(parser1.parse("0") == NaturalLiteral(BigInt(0)))
+    expect(parser1.parse("123") == NaturalLiteral(BigInt(123)))
+    expect(parser1.parse("123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890") == NaturalLiteral(BigInt("123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890")))
   }
 
   test("parse variable") {
-    expect(parser1.parseToExpression("x") == Variable(VarName("x"), BigInt(0)))
-    expect(parser1.parseToExpression("x @ 0") == Variable(VarName("x"), BigInt(0)))
-    expect(parser1.parseToExpression("x @ 10") == Variable(VarName("x"), BigInt(10)))
+    expect(parser1.parse("x") == Variable(VarName("x"), BigInt(0)))
+    expect(parser1.parse("x @ 0") == Variable(VarName("x"), BigInt(0)))
+    expect(parser1.parse("x @ 10") == Variable(VarName("x"), BigInt(10)))
   }
 
   test("parse addition") {
-    expect(parser1.parseToExpression("0 + 1") == Operator(NaturalLiteral(BigInt(0)), NanoOperator.Plus, NaturalLiteral(BigInt(1))))
+    expect(parser1.parse("0 + 1") == Operator(NaturalLiteral(BigInt(0)), NanoOperator.Plus, NaturalLiteral(BigInt(1))))
   }
 
   test("parse lambda") {
-    expect(parser1.parseToExpression("\\(x : Natural) -> x + 1") == Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Variable(VarName("x"), 0), NanoOperator.Plus, NaturalLiteral(1))))
+    expect(parser1.parse("\\(x : Natural) -> x + 1") == Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Variable(VarName("x"), 0), NanoOperator.Plus, NaturalLiteral(1))))
   }
 
 
   test("parse lambda and applications") {
-    expect(parser1.parseToExpression("(\\(x : Natural) -> f x + 1) 2") == Application(Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Application(Variable(VarName("f"), 0), Variable(VarName("x"), 0)), NanoOperator.Plus, NaturalLiteral(1))), NaturalLiteral(2)))
+    expect(parser1.parse("(\\(x : Natural) -> f x + 1) 2") == Application(Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Application(Variable(VarName("f"), 0), Variable(VarName("x"), 0)), NanoOperator.Plus, NaturalLiteral(1))), NaturalLiteral(2)))
   }
 
   test("parse forall") {
-    expect(parser1.parseToExpression("(\\(x : Natural) -> x + 1) : forall (x : Natural) -> Natural") == Annotation(Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Variable(VarName("x"), 0), NanoOperator.Plus, NaturalLiteral(1))), Forall(VarName("x"), Constant(NanoConstant.Natural), Constant(NanoConstant.Natural))))
+    expect(parser1.parse("(\\(x : Natural) -> x + 1) : forall (x : Natural) -> Natural") == Annotation(Lambda(VarName("x"), Constant(NanoConstant.Natural), Operator(Variable(VarName("x"), 0), NanoOperator.Plus, NaturalLiteral(1))), Forall(VarName("x"), Constant(NanoConstant.Natural), Constant(NanoConstant.Natural))))
   }
 }
