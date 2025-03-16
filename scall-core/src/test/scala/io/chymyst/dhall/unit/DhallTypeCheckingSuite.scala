e22e458dd22526c8e200ddd1f54f7eadf25834b8
Sergei Winitzki <winitzki@gmail.com>
2025-03-16 22:05:22 +0100

    reformat

--- scall-core/src/test/scala/io/chymyst/dhall/unit/DhallTypeCheckingSuite.scala
+++ scall-core/src/test/scala/io/chymyst/dhall/unit/DhallTypeCheckingSuite.scala
@@ -31,7 +31,8 @@ class DhallTypeCheckingSuite extends DhallTest {
         expect(x.print == y.print, x == y)
         file.getName
       }
-      if (result.isFailure) println(s"DhallTypeCheckingSuite failure: ${file.getPath}: ${result.failed.get.getMessage}") // \n${Throwables.printThrowable(result.failed.get)})
+      if (result.isFailure)
+        println(s"DhallTypeCheckingSuite failure: ${file.getPath}: ${result.failed.get.getMessage}") // \n${Throwables.printThrowable(result.failed.get)})
       result
     }
     TestUtils.requireSuccessAtLeast(364, results)
